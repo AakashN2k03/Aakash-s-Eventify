07376b5cf543af6f6da94545a44e902599f647b2
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2024-10-13 17:32:28 +0530

    Update SignUpForm.js

--- mern_project/src/components/SignUpForm.js
+++ mern_project/src/components/SignUpForm.js
@@ -13,7 +13,7 @@ function SignUpForm() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:3000/registration', { name, email, password });
+            const response = await axios.post('https://aakash-s-eventify-backend.onrender.com/registration', { name, email, password });
             console.log(response);
             navigate('/home');
         } catch (err) {
